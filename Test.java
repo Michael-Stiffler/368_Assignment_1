04e57fb2740f0865de6f7b6d2368bec15ac7b58d
vilqo <56851676+vilqo@users.noreply.github.com>
2022-02-08 15:58:29 -0500

    Started
    Started

---
+++ Test.java
@@ -0,0 +1,19 @@
+public class Test {
+    public static void main(String[] args) {
+        char[][] board = new char[3][3];
+    }
+
+    public static void printgameBoard(char[][] gameBoard) {
+
+    }
+
+    public static void placeInputPosition(char[][] gameBoard, int pos, String user) {
+
+    }
+
+    public static String checkWinner() {
+        String winner = "";
+        return winner;
+    }
+
+}
