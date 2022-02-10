cc39f414d55c854fb2f1e8eb90ead31b12de5243
vilqo <56851676+vilqo@users.noreply.github.com>
2022-02-10 01:03:44 -0500

    Additions and Fixes
    Added logic for game loop. Made it look cleaner and added some comments. Also added some methods for the computer player

--- Test.java
+++ Test.java
@@ -8,16 +8,82 @@ public class Test {
                 { ' ', ' ', ' ' },
                 { ' ', ' ', ' ' } };
 
+        int userPositionSelected = 0;
+        int computerPositionSelected = 0;
+        boolean gameOver = false;
+
         // Value can either be Person or Computer. Should also start as Person.
         String currentPlayer = "Person";
+
+        //print the board at the start
         printgameBoard(board);
-        boolean gameOver = false;
-        int currentPositionSelected = 0;
 
         while (!gameOver) {
             if (currentPlayer == "Person") {
+                userPositionSelected = getInputFromUser(board);
+                placeInputPosition(board, userPositionSelected, currentPlayer);
+                // currentPlayer = "Computer";
+            } else {
+                //TODO
 
-                Scanner scanner = new Scanner(System.in);
+                //Make it so the computer can make a move
+                computerPositionSelected = generateComputerMove(board);
+                
+                placeInputPosition(board, computerPositionSelected, currentPlayer);
+                //currentPlayer = "Person";
+            }
+
+            //always print the board before checking if the game ends
+            printgameBoard(board);
+
+            //checkWinner();
+        }
+    }
+
+    public static void printgameBoard(char[][] gameBoard) {
+        //This method is complete
+
+        System.out.println("+-----------+");
+        System.out.println("| " + gameBoard[0][0] + " | " + gameBoard[0][1] + " | " + gameBoard[0][2] + " |");
+        System.out.println("|-----------|");
+        System.out.println("| " + gameBoard[1][0] + " | " + gameBoard[1][1] + " | " + gameBoard[1][2] + " |");
+        System.out.println("|-----------|");
+        System.out.println("| " + gameBoard[2][0] + " | " + gameBoard[2][1] + " | " + gameBoard[2][2] + " |");
+        System.out.println("+-----------+");
+        System.out.println();
+
+    }
+
+    public static void placeInputPosition(char[][] gameBoard, int pos, String user) {
+        // This method is complete
+
+        if (user == "Person") {
+            int[] position = indexOf2DArray(pos);
+            gameBoard[position[0]][position[1]] = 'X';
+        } else {
+
+        }
+    }
+
+    public static String checkWinner() {
+        String winner = "";
+        return winner;
+    }
+
+
+    public static int generateComputerMove(char[][] board) {
+        int currentComputerPositionSelected = 0;
+        return currentComputerPositionSelected;
+    }
+
+    public static int getInputFromUser(char[][] board) {
+        //This method is complete
+
+        //Implements the flow to get what position the user wants
+        //and checks if that spot is taken or not 
+        
+        int currentPositionSelected = 0;
+        Scanner scanner = new Scanner(System.in);
                 boolean pickedAValidPos = false;
 
                 while (!pickedAValidPos) {
@@ -33,42 +99,15 @@ public class Test {
                         System.out.println("Try again!");
                     }
                 }
-                placeInputPosition(board, currentPositionSelected, currentPlayer);
-                // currentPlayer = "Computer";
-            } else {
-                currentPlayer = "Person";
-            }
-            printgameBoard(board);
-        }
-    }
-
-    public static void printgameBoard(char[][] gameBoard) {
-        System.out.println("+-----------+");
-        System.out.println("| " + gameBoard[0][0] + " | " + gameBoard[0][1] + " | " + gameBoard[0][2] + " |");
-        System.out.println("|-----------|");
-        System.out.println("| " + gameBoard[1][0] + " | " + gameBoard[1][1] + " | " + gameBoard[1][2] + " |");
-        System.out.println("|-----------|");
-        System.out.println("| " + gameBoard[2][0] + " | " + gameBoard[2][1] + " | " + gameBoard[2][2] + " |");
-        System.out.println("+-----------+");
-        System.out.println();
-
-    }
-
-    public static void placeInputPosition(char[][] gameBoard, int pos, String user) {
-        if (user == "Person") {
-            int[] position = indexOf2DArray(pos);
-            gameBoard[position[0]][position[1]] = 'X';
-        } else {
-
-        }
-    }
-
-    public static String checkWinner() {
-        String winner = "";
-        return winner;
+        return currentPositionSelected;
     }
 
     public static int[] indexOf2DArray(int pos) {
+        // This method is complete
+
+        // This is just used to turn a position the user enters
+        // like "9" into board[3][3]
+
         int index = 1;
         int[] position = new int[2];
 
